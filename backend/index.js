052b8d43c2b0a7a3cfb048f981fbcb26ef81d228
Shyam-7865 <engr.shyamkumar7@gmail.com>
2026-03-03 20:19:21 +0530

    Chat application project completed

--- backend/index.js
+++ backend/index.js
@@ -33,8 +33,9 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    // Explicit string catch-all
+    app.get("/*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
     });
 
 }
